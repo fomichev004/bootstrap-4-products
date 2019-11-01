077b1704c4a24663e3065484f2dd66b82ccc74ef
FOMYCHEV (work_pc) <Maksym.Fomychev@agroscope.ua>
2019-11-01 15:29:00 +0200

    Create String.cs

---
+++ String.cs
@@ -0,0 +1,21 @@
+
+#//public string Name;
+#
+#//string input
+#
+#//string output = Name.ToTitleCase;
+#
+#//Trim // Trim убрать лишние пробелы в строке 
+#// https://www.youtube.com/watch?v=vNJP7PIx4DM&list=PL05SB3rBbUsraqiEUeS70RKhVAu97nGeb&index=22
+#// 20:00. 28:00
+
+#//python
+NameDriver_str = input('Введите ФИО')
+
+print ( '{}'.format( NameDriver_str ).title().strip() )
+
+Car_str = input('Введите номер машины')
+print ( '{}'.format( Car_str ).upper().replace(" ", " / ").replace("-", "/").replace("  ", " ").strip() )
+
+Number_int = input('Телефон')
+print ( '{}'.format( Number_int ).strip() )
